feb98a86162f382c53e9643598b9193271e22014
delchev <delchevn@gmail.com>
2022-07-27 12:43:56 +0300

    applying the new i&f

--- ide-debugger/views/debugger/controller.js
+++ ide-debugger/views/debugger/controller.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2020 SAP and others.
+ * Copyright (c) 2010-2022 SAP and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -9,77 +9,57 @@
  *   SAP - initial API and implementation
  */
 
-angular.module('debugger', ['ngAnimate', 'ngSanitize', 'ui.bootstrap'])
-	.factory('httpRequestInterceptor', function () {
-		let csrfToken = null;
-		return {
-			request: function (config) {
-				config.headers['X-Requested-With'] = 'Fetch';
-				config.headers['X-CSRF-Token'] = csrfToken ? csrfToken : 'Fetch';
-				return config;
-			},
-			response: function (response) {
-				let token = response.headers()['x-csrf-token']
-				if (token) {
-					csrfToken = token;
-				}
-				return response;
-			}
-		};
-	})
-	.config(['$httpProvider', function ($httpProvider) {
-		$httpProvider.interceptors.push('httpRequestInterceptor');
-		//check if response is error. errors currently are non-json formatted and fail too early
-		$httpProvider.defaults.transformResponse.unshift(function (data, headersGetter, status) {
-			if (status > 399) {
-				data = {
-					"error": data
-				}
-				data = JSON.stringify(data);
-			}
-			return data;
-		});
-	}])
-	.factory('$messageHub', [function () {
-		let messageHub = new FramesMessageHub();
-		let message = function (evtName, data) {
-			messageHub.post({ data: data }, 'debugger.' + evtName);
-		};
+const debuggerView = angular.module('debugger', ['ideUI', 'ideView']);
 
-		let on = function (topic, callback) {
-			messageHub.subscribe(callback, topic);
-		};
+debuggerView.config(["messageHubProvider", function (messageHubProvider) {
+	messageHubProvider.eventIdPrefix = 'debugger-view';
+}]);
 
-		return {
-			message: message,
-			on: on
-		};
-	}])
-	.controller('DebuggerController', ['$scope', '$messageHub', function ($scope, $messageHub) {
+debuggerView.filter('trusted', ['$sce', function ($sce) {
+	return function (url) {
+		return $sce.trustAsResourceUrl(url);
+	};
+}]);
 
-		let protocol = window.location.protocol === "http:" ? "ws" : "wss";
-		let hostPortIndexOf = window.location.host.indexOf(":");
-		let host = hostPortIndexOf > 0 ? window.location.host.substring(0, hostPortIndexOf) : window.location.host;
-		let devToolsLocation = "/services/v4/web/dev-tools/js_app.html"
-		// TODO: The debug port can be configured
-		let debugPort = 8081;
-		let debuggerLocation = devToolsLocation + "?" + protocol + "=" + host + ":" + debugPort + "/debug";
+debuggerView.controller('DebuggerController', ['$scope', 'messageHub', function ($scope, messageHub) {
 
-		function refreshDebugger(resourcePath) {
-			if ($scope.previewUrl == null) {
-				$scope.previewUrl = debuggerLocation;// + resourcePath;
-				let tokenParam = 'refreshToken=' + new Date().getTime();
-				$scope.previewUrl += ($scope.previewUrl.indexOf('?') > 0 ? ($scope.previewUrl.endsWith('?') ? tokenParam : ('&' + tokenParam)) : ('?' + tokenParam));
-				$scope.$apply();
-			}
+
+	// .factory('$messageHub', [function () {
+	// 	let messageHub = new FramesMessageHub();
+	// 	let message = function (evtName, data) {
+	// 		messageHub.post({ data: data }, 'debugger.' + evtName);
+	// 	};
+
+	// 	let on = function (topic, callback) {
+	// 		messageHub.subscribe(callback, topic);
+	// 	};
+
+	// 	return {
+	// 		message: message,
+	// 		on: on
+	// 	};
+	// }])
+	// .controller('DebuggerController', ['$scope', '$messageHub', function ($scope, $messageHub) {
+
+	let protocol = window.location.protocol === "http:" ? "ws" : "wss";
+	let hostPortIndexOf = window.location.host.indexOf(":");
+	let host = hostPortIndexOf > 0 ? window.location.host.substring(0, hostPortIndexOf) : window.location.host;
+	let devToolsLocation = "/services/v4/web/dev-tools/js_app.html"; // "devtools://devtools/bundled/js_app.html";
+	// TODO: The debug port can be configured
+	let debugPort = 8081;
+	let debuggerLocation = devToolsLocation + "?" + protocol + "=" + host + ":" + debugPort + "/debug";
+
+	function refreshDebugger(resourcePath) {
+		if ($scope.previewUrl == null || $scope.previewUrl == undefined) {
+			$scope.previewUrl = debuggerLocation;// + resourcePath;
+			let tokenParam = 'refreshToken=' + new Date().getTime();
+			$scope.previewUrl += ($scope.previewUrl.indexOf('?') > 0 ? ($scope.previewUrl.endsWith('?') ? tokenParam : ('&' + tokenParam)) : ('?' + tokenParam));
+			$scope.$apply();
 		}
-		$messageHub.on('workspace.file.selected', function (msg) {
-			let resourcePath = msg.data.path.substring(msg.data.path.indexOf('/', 1));
-			refreshDebugger(resourcePath);
-		}.bind(this));
+	}
+	messageHub.onFileSelected((fileDescriptor) => {
+		let resourcePath = fileDescriptor.path.substring(fileDescriptor.path.indexOf('/', 1));
+		refreshDebugger(resourcePath);
+	});
 
-		$messageHub.on('debugger.refresh', function (msg) {
-			refreshDebugger(msg.data.resourcePath);
-		});
-
-	}]);
+}]);
--- ide-debugger/views/debugger/view.js
+++ ide-debugger/views/debugger/view.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2020 SAP and others.
+ * Copyright (c) 2010-2022 SAP and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -8,13 +8,17 @@
  * Contributors:
  *   SAP - initial API and implementation
  */
-exports.getView = function () {
-	return {
-		"id": "debugger",
-		"name": "Debugger",
-		"factory": "frame",
-		"region": "center-top",
-		"label": "Debugger",
-		"link": "../ide-debugger/views/debugger/index.html"
-	};
+
+const viewData = {
+	id: "debugger",
+	name: "Debugger",
+	factory: "frame",
+	region: "center-top",
+	label: "Debugger",
+	link: "../ide-debugger/views/debugger/index.html"
 };
+if (typeof exports !== 'undefined') {
+	exports.getView = function () {
+		return viewData;
+	}
+}
